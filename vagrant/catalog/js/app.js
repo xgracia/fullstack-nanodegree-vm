9537a0a6d44ea733d4b76cdb32a0f6d69fff4d9e
Gracia, Xavier (xg146r) <xg146r@att.com>
2017-09-04 18:52:41 -0500

    helper function for unique IDs

--- vagrant/catalog/js/app.js
+++ vagrant/catalog/js/app.js
@@ -1,3 +1,14 @@
 (function(){
     window.app = window.app || {};
+
+    /* Helper Functions */
+    app.helpers = {
+        uuidv4: function(){
+            /* https://stackoverflow.com/a/2117523 */
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+                return v.toString(16);
+            });
+        }
+    };
 })();
