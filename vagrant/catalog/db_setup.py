309558b79f444913d9c31f3e570657a5b207e821
Gracia, Xavier (xg146r) <xg146r@att.com>
2018-02-07 23:26:19 -0600

    adding str methods for the db models

--- vagrant/catalog/db_setup.py
+++ vagrant/catalog/db_setup.py
@@ -9,6 +9,9 @@ class Category(Base):
     __tablename__ = 'categories'
     id = Column(Integer, primary_key=True)
     category = Column(String(75), nullable = False)
+    
+    def __str__(self):
+        return self.category
 
 class CatalogItem(Base):
     __tablename__ = 'catalog_items'
@@ -17,6 +20,9 @@ class CatalogItem(Base):
     description = Column(String(255))
     category_id = Column(Integer, ForeignKey('categories.id'))
     category = relationship(Category)
+
+    def __str__(self):
+        return self.item_name
 
 engine = create_engine('postgresql:///catalog')
 Base.metadata.create_all(engine)
